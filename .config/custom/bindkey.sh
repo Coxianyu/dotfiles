43e96928ccbef1661c54e0b8df7b9923cf5bc705
xianyu <xianyuwhoami@gmail.com>
2021-10-31 10:48:26 +0800

    fix(bindkey.sh): 回退 bindkey 到 commit c856ed148ae78dd9ee5aa7a0fa484d40aa79112a
    当前 bindkey.sh 会导致 ctrl+key 的快捷键失效

--- .config/custom/bindkey.sh
+++ .config/custom/bindkey.sh
@@ -1,11 +1,1 @@
-bindkey -r '^L'
-bindkey -r '^H'
-bindkey -r '^J'
-bindkey -r '^B'
-# bindkey -r '^Q'
-# bindkey '^Q' forward-word
-bindkey '^A' beginning-of-line
-bindkey '^E' end-of-line
-bindkey '^B' kill-line
 bindkey '^N' clear-screen
-bindkey '^F' forward-char
