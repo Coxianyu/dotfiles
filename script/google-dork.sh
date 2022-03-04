43429dbce5cb9eeb8a0aa3342794b8c7498e03af
xianyu <xianyuwhoami@gmail.com>
2022-03-04 20:03:48 +0800

    2022年 03月 04日 星期五 20:03:48 CST

--- script/google-dork.sh
+++ script/google-dork.sh
@@ -1,50 +1,51 @@
+read -p "enter the keyword: " keyword
 echo 在线 IDE:可能找到硬编码的凭证
-echo https://www.google.com/search?q=site:codepad.co%20"$1"
+echo https://www.google.com/search?q=site:codepad.co%20"$keyword"
 
 echo  有可能找到员工上传的明文密码
-echo https://www.google.com/search?q=site:scribd.com%20"$1"
+echo https://www.google.com/search?q=site:scribd.com%20"$keyword"
 
 echo  寻找公司上传的 nodejs 源码
-echo https://www.google.com/search?q=site:npmjs.com%20"$1"
+echo https://www.google.com/search?q=site:npmjs.com%20"$keyword"
 
 echo  寻找公司上传的 nodejs 源码
-echo https://www.google.com/search?q=site:npm.runkit.com%20"$1"
+echo https://www.google.com/search?q=site:npm.runkit.com%20"$keyword"
 
-echo https://www.google.com/search?q=site:libraries.io%20"$1"
+echo https://www.google.com/search?q=site:libraries.io%20"$keyword"
 
 echo  在线脑图， 可能找到内部的流程图等东西
-echo https://www.google.com/search?q=site:coggle.it%20"$1"
+echo https://www.google.com/search?q=site:coggle.it%20"$keyword"
 
 echo  用于存储书签和链接， 有可能找到内部链接， 文档
-echo https://www.google.com/search?q=site:papaly.com%20"$1"
+echo https://www.google.com/search?q=site:papaly.com%20"$keyword"
 
 echo  web 看板， 有可能找到凭证
-echo https://www.google.com/search?q=site:trello.com%20"$1"
+echo https://www.google.com/search?q=site:trello.com%20"$keyword"
 
 echo  用于制作 ppt， 有可能有凭证
-echo https://www.google.com/search?q=site:prezi.com%20"$1"
+echo https://www.google.com/search?q=site:prezi.com%20"$keyword"
 
 echo  NPM 和 Github 的 CDN
-echo https://www.google.com/search?q=site:jsdelivr.net%20"$1"
+echo https://www.google.com/search?q=site:jsdelivr.net%20"$keyword"
 
 echo  在线的工具， 用于测试前端代码， 可能有凭据
-echo https://www.google.com/search?q=site:codepen.io%20"$1"
+echo https://www.google.com/search?q=site:codepen.io%20"$keyword"
 
 echo  上传文本的地方， 有可能有文档
-echo https://www.google.com/search?q=site:pastebin.com%20"$1"
+echo https://www.google.com/search?q=site:pastebin.com%20"$keyword"
 
 echo  在线编译器， 可能在用户脚本中找到 api 等东西
-echo https://www.google.com/search?q=site:repl.it%20"$1"
+echo https://www.google.com/search?q=site:repl.it%20"$keyword"
 
-echo https://www.google.com/search?q=site:gitter.im "$1"
+echo https://www.google.com/search?q=site:gitter.im "$keyword"
 echo  是一个开源消息传递平台。您有时可以找到包含凭据、内部链接和其他信息的私人消息
 
-echo https://www.google.com/search?q=site:gitter.im%20"$1"
+echo https://www.google.com/search?q=site:gitter.im%20"$keyword"
 echo  同样是放置源码的地方， 可能有硬编码的凭据
-echo https://www.google.com/search?q=site:bitbucket.org%20"$1"
+echo https://www.google.com/search?q=site:bitbucket.org%20"$keyword"
 
 echo  寻找包含敏感信息的产品
-echo https://www.google.com/search?q=site:*.atlassian.net%20"$1"
+echo https://www.google.com/search?q=site:*.atlassian.net%20"$keyword"
 
 echo  同样是放置源码的地方
-echo https://www.google.com/search?q=Inurl:gitlab%20"$1"
+echo https://www.google.com/search?q=Inurl:gitlab%20"$keyword"
